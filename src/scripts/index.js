b1d52177a4143b0da93ec2759a711300aa83a5b3
Luthfi Taufiqulhakim <luthfit64@gmail.com>
2024-06-11 21:47:19 +0700

    make map.html and some image of map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
+import '../styles/responsivehp.css';
+import '../styles/map.css';
 import "./component/footer-component.js";
 
 
@@ -30,3 +33,33 @@ import "./component/footer-component.js";
 
 // });
  
+
+let navbar = document.querySelector(".navbar");
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+
+
